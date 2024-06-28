98937c566c11a2a9d37b7ad73deecc6370c71b14
YukeChan2002 <yukeshchandran002@gmail.com>
2024-06-28 20:11:55 +0530

    last

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Landtwo from "./Landtwo";
 
 const Landing = (props) => {
   return (
     <div>
       Landing
       <p>The car is {props.color}</p>
+      <Landtwo colour={props.color}/>
     </div>
   );
 };
---
+++ src/Landtwo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landtwo = (props) => {
+    console.log(props);
+  return (
+    <div>Landtwo
+        <p>My new virtus color is {props.colour}</p>
+    </div>
+  )
+}
+
+export default Landtwo
